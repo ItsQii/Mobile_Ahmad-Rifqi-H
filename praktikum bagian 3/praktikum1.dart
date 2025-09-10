7bedcc95d935e660055a4b6e45c6f1e55ecd45e4
Ripqii <arsya7926@gmail.com>
2025-09-10 12:16:12 +0700

    langkah 3

--- praktikum bagian 3/praktikum1.dart
+++ praktikum bagian 3/praktikum1.dart
@@ -1,11 +1,10 @@
 void main() {
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
+  final List<String?> list = List.filled(5, null, growable: false);
 
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  list[1] = "Ahmad Rifqi Hendriansyah";  // Nama
+  list[2] = "2341720134";     // NIM
+
+  print(list);
 }
+
+
